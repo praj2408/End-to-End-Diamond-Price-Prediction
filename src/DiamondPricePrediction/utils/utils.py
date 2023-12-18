445c24fee9cd32dcb95438e99e7ffce1e5c13211
praj2408 <prajwalgbdr03@gmail.com>
2023-12-18 01:15:28 -1200

    utils added

--- src/DiamondPricePrediction/utils/utils.py
+++ src/DiamondPricePrediction/utils/utils.py
@@ -20,3 +20,24 @@ def save_object(file_path, obj):
         pass
     except Exception as e:
         raise CustomException(e, sys)
+    
+    
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            
+            model.fit(X_train, y_train)
+            
+            y_test_pred = model.predict(X_test)
+            
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+            return report
+    except Exception as e:
+        raise CustomException(e, sys)
