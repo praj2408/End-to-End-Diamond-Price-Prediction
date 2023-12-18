936ca71dcdca139e74608bc2b3e10acd8655dafb
praj2408 <prajwalgbdr03@gmail.com>
2023-12-18 01:15:19 -1200

    training pipeline fixed

--- src/DiamondPricePrediction/pipelines/training_pipeline.py
+++ src/DiamondPricePrediction/pipelines/training_pipeline.py
@@ -1,4 +1,7 @@
 from src.DiamondPricePrediction.components.data_ingestion import DataIngestion
+from src.DiamondPricePrediction.components.data_transformation import DataTransformation
+from src.DiamondPricePrediction.components.model_trainer import ModelTrainer
+
 
 import os, sys
 
@@ -9,8 +12,12 @@ import pandas as pd
 
 obj = DataIngestion()
 
-obj.initiate_data_ingestion()
+train_data_path, test_data_path=obj.initiate_data_ingestion()
+
+data_trainformation = DataTransformation()
+
+train_arr, test_arr = data_trainformation.initiate_data_transformation(train_data_path, test_data_path)
 
 
-
-
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initiate_model_training(train_arr, test_arr)
