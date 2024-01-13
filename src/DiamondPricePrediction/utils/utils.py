a8621ca496833135d80b004a15886cb37804e32e
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-01-13 19:09:09 +0530

    load object added

--- src/DiamondPricePrediction/utils/utils.py
+++ src/DiamondPricePrediction/utils/utils.py
@@ -41,3 +41,13 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
             return report
     except Exception as e:
         raise CustomException(e, sys)
+    
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
