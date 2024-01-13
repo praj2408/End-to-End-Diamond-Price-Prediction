0c721200aadb61c5232f65df743b8edc53cffbec
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-01-13 19:02:25 +0530

    upload

---
+++ src/DiamondPricePrediction/pipelines/training_pipeline.py
@@ -0,0 +1,23 @@
+from src.DiamondPricePrediction.components.data_ingestion import DataIngestion
+from src.DiamondPricePrediction.components.data_transformation import DataTransformation
+from src.DiamondPricePrediction.components.model_trainer import ModelTrainer
+
+
+import os, sys
+
+from src.DiamondPricePrediction.logger import logging
+from src.DiamondPricePrediction.exception import CustomException
+
+import pandas as pd
+
+obj = DataIngestion()
+
+train_data_path, test_data_path=obj.initiate_data_ingestion()
+
+data_trainformation = DataTransformation()
+
+train_arr, test_arr = data_trainformation.initiate_data_transformation(train_data_path, test_data_path)
+
+
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initiate_model_training(train_arr, test_arr)
---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+
+
+path = "notebooks/reasearch.ipynb"
