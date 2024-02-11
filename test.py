715fd8bfafd3bf060acd07761ab01edb02c2302d
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-02-11 08:52:40 +0530

    update

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-import os
+import mlflow
 
+calculate = lambda x, y: x% y
 
-path = "notebooks/reasearch.ipynb"
+if __name__ == "__main__":
+    with mlflow.start_run():
+        x,y = 100, 200
+        result = calculate(x,y)
+        print(f"here is my result {result}")
+        mlflow.log_param("x", x)
+        mlflow.log_param("y", y)
+        mlflow.log_param("result", result)
