9ebdece4e8de7c21355dd16451502da1f7f0bcb1
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-02-11 23:28:41 +0530

    update

--- src/DiamondPricePrediction/pipelines/training_pipeline.py
+++ src/DiamondPricePrediction/pipelines/training_pipeline.py
@@ -1,6 +1,8 @@
 from src.DiamondPricePrediction.components.data_ingestion import DataIngestion
 from src.DiamondPricePrediction.components.data_transformation import DataTransformation
 from src.DiamondPricePrediction.components.model_trainer import ModelTrainer
+from src.DiamondPricePrediction.components.model_evaluation import ModelEvaluation
+
 
 
 import os, sys
@@ -21,3 +23,6 @@ train_arr, test_arr = data_trainformation.initiate_data_transformation(train_dat
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr, test_arr)
+
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr, test_arr)
